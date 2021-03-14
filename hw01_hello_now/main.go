97665a70cea8263a140f8e2c5630bc1448f59a82
Raschudesny <rifkinsm@gmail.com>
2021-03-14 08:10:37 +0300

    fix typo

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -12,7 +12,7 @@ func main() {
 	systemTime := time.Now()
 	ntpTime, err := ntp.Time("1.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		log.Fatalf("Erorr during reading time from NTP server: %v", err)
+		log.Fatalf("Error during reading time from NTP server: %v", err)
 	}
 
 	fmt.Printf("current time: %v\n", systemTime.Round(time.Microsecond)) //nolint:forbidigo
