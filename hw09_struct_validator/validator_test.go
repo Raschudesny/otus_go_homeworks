17139be33cdc8547a9a87485902141cee1019a14
Raschudesny <rifkinsm@gmail.com>
2021-04-07 04:15:15 +0300

    Hw09 struct validator (#10)
    * wip
    
    * hw09 implemented

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,9 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -36,25 +37,81 @@ type (
 	}
 )
 
-func TestValidate(t *testing.T) {
-	tests := []struct {
-		in          interface{}
-		expectedErr error
+func TestValidateStruct(t *testing.T) {
+	for _, testData := range [...]struct {
+		name           string
+		input          interface{}
+		expectedErrors []error
 	}{
 		{
-			// Place your code here.
+			"str length rule all valid",
+			App{"12345"},
+			nil,
 		},
-		// ...
-		// Place your code here.
-	}
-
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
+		{
+			"str length rule field invalid",
+			App{"123456"},
+			[]error{ErrStrLengthRuleIsInvalid},
+		},
+		{
+			"int in rule with multiple values all valid",
+			Response{200, "123"},
+			nil,
+		},
+		{
+			"int in rule with multiple values not valid",
+			Response{123123123213, "yo"},
+			[]error{ErrIntInRuleIsInvalid},
+		},
+		{
+			"no validation rules provided",
+			Token{
+				[]byte{1, 2, 3, 4, 5},
+				[]byte{1, 2, 3, 4, 5},
+				[]byte{1, 2, 3, 4, 5},
+			},
+			nil,
+		},
+		{
+			"str length and str in rule is invalid at the same time",
+			User{
+				"123",
+				"name",
+				20,
+				"kek@mail.ru",
+				"admine",
+				[]string{"89261232323", "89263212121", "89261242424"},
+				[]byte{1, 2, 3, 4, 5},
+			},
+			[]error{ErrStrLengthRuleIsInvalid, ErrStrInRuleIsInvalid},
+		},
+		{
+			"len and max rule not valid",
+			User{
+				"123",
+				"name",
+				600,
+				"kek@mail.ru",
+				"admin",
+				[]string{"89261232323", "89263212121", "89261242424"},
+				[]byte{1, 2, 3, 4, 5},
+			},
+			[]error{ErrStrLengthRuleIsInvalid, ErrIntMaxRuleIsInvalid},
+		},
+	} {
+		t.Run(testData.name, func(t *testing.T) {
+			testData := testData
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			var validationErrors ValidationErrors
+			errs := ValidateStruct(testData.input)
+			if len(testData.expectedErrors) == 0 {
+				require.NoError(t, errs)
+				return
+			}
+			require.ErrorAs(t, errs, &validationErrors)
+			for i, e := range validationErrors {
+				require.ErrorIs(t, e.Err, testData.expectedErrors[i])
+			}
 		})
 	}
 }
